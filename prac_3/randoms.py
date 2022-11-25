3d826cc14400b433b279ffc8b2bb020fff3dc672
Benelyonchoo <94026597+Benelyonchoo@users.noreply.github.com>
2022-11-25 17:53:02 +0800

    random functions

---
+++ prac_3/randoms.py
@@ -0,0 +1,22 @@
+
+# What did you see on line 1?
+# Number 7.
+# What was the smallest number you could have seen, what was the largest?
+# Smallest number is 5 and largest 20.
+
+# What did you see on line 2?
+# Number 9.
+# What was the smallest number you could have seen, what was the largest?
+# Smallest number 3 and largest 9.
+# Could line 2 have produced a 4?
+# No.
+
+# What did you see on line 3?
+# Number 3.5737298041175376
+# What was the smallest number you could have seen, what was the largest?
+# Smallest 2.5852796585248536 and largest a 5.484120402706887
+
+
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+import random
+print(random.randint(1,100))
